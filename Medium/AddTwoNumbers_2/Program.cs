e0540ca437ce9302d14a6ab181d35f937b049136
isftobsuperfluous <a.kudryavcev8080@gmail.com>
2023-01-25 13:43:52 +0300

    plus 2 solved tasks

---
+++ Medium/AddTwoNumbers_2/Program.cs
@@ -0,0 +1,68 @@
+﻿//var ll1 = new ListNode(9);
+//ll1.next = new ListNode(9);
+//ll1.next.next = new ListNode(9);
+//ll1.next.next.next = new ListNode(9);
+//ll1.next.next.next.next = new ListNode(9);
+//ll1.next.next.next.next.next = new ListNode(9);
+//ll1.next.next.next.next.next.next = new ListNode(9);
+
+//var ll2 = new ListNode(9);
+//ll2.next = new ListNode(9);
+//ll2.next.next = new ListNode(9);
+//ll2.next.next.next = new ListNode(9);
+
+var ll1 = new ListNode(9);
+ll1.next = new ListNode(9);
+ll1.next.next = new ListNode(1);
+
+var ll2 = new ListNode(1);
+
+var result = ListNode.AddTwoNumbers(ll1, ll2);
+var current = result;
+while (current is not null)
+{
+	Console.WriteLine(current.val);
+	current = current.next;
+}
+
+public class ListNode
+{
+	public int val { get; set; }
+	public ListNode next { get; set; }
+
+	public ListNode(int val = 0, ListNode next = null)
+	{
+		this.val = val; this.next = next;
+	}
+
+	public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+	{
+		var current = new ListNode();
+		ListNode head = current;
+		var sum = 0;
+		while (l1 is not null || l2 is not null)
+		{
+			sum /= 10;
+			if (l1 is not null)
+			{
+				sum += l1.val;
+				l1 = l1.next;
+			}
+			if (l2 is not null)
+			{
+				sum += l2.val;
+				l2 = l2.next;
+			}
+
+			current.next = new ListNode(sum % 10);
+			current = current.next;
+		}
+
+		if (sum / 10 == 1)
+		{
+			current.next = new ListNode(1);
+		}
+
+		return head.next;
+	}
+}
---
+++ Medium/LongestSubstringWithoutRepeatingCharacters_3/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.Text;
+
+Console.Write(LengthOfLongestSubstring("pwwkew"));
+Console.Write(LengthOfLongestSubstringBadSolution("pwwkew"));
+
+int LengthOfLongestSubstringBadSolution(string s)
+{
+	if (s.Length < 2)
+	{
+		return s.Length;
+	}
+
+	var storage = new Dictionary<char, int>();
+	var sb = new StringBuilder();
+	var maxLength = 0;
+	var length = 0;
+	for (int i = 0; i < s.Length; i++)
+	{
+		for (int j = i; j < s.Length; j++)
+		{
+			if (storage.TryAdd(s[j], 1))
+			{
+				sb.Append(s[j]);
+				length++;
+			}
+			else
+			{
+				storage = new Dictionary<char, int>();
+				if (length > maxLength)
+				{
+					maxLength = length;
+				}
+				length = 0;
+				sb.Clear();
+				break;
+			}
+		}
+	}
+
+	return maxLength;
+}
+
+int LengthOfLongestSubstring(string s)
+{
+	if (s.Length < 2)
+	{
+		return s.Length;
+	}
+
+	int i = 0, j = 0, max = 0;
+	var storage = new HashSet<char>();
+	while (j < s.Length)
+	{
+		if (!storage.Contains(s[j]))
+		{
+			storage.Add(s[j++]);
+			max = Math.Max(max, storage.Count);
+		}
+		else
+		{
+			storage.Remove(s[i++]);
+		}
+	}
+
+	return max;
+}
