2e4665ed1f9c2152cb9e921fa980e59d967132cb
isftobsuperfluous <a.kudryavcev8080@gmail.com>
2023-01-25 13:47:02 +0300

    microfix

--- Medium/LongestSubstringWithoutRepeatingCharacters_3/Program.cs
+++ Medium/LongestSubstringWithoutRepeatingCharacters_3/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Text;
-
-Console.Write(LengthOfLongestSubstring("pwwkew"));
+﻿Console.Write(LengthOfLongestSubstring("pwwkew"));
 Console.Write(LengthOfLongestSubstringBadSolution("pwwkew"));
 
 int LengthOfLongestSubstringBadSolution(string s)
@@ -10,28 +8,26 @@ int LengthOfLongestSubstringBadSolution(string s)
 		return s.Length;
 	}
 
-	var storage = new Dictionary<char, int>();
-	var sb = new StringBuilder();
+	var storage = new HashSet<char>();
 	var maxLength = 0;
 	var length = 0;
 	for (int i = 0; i < s.Length; i++)
 	{
 		for (int j = i; j < s.Length; j++)
 		{
-			if (storage.TryAdd(s[j], 1))
+			if (!storage.Contains(s[j]))
 			{
-				sb.Append(s[j]);
+				storage.Add(s[j]);
 				length++;
 			}
 			else
 			{
-				storage = new Dictionary<char, int>();
+				storage = new HashSet<char>();
 				if (length > maxLength)
 				{
 					maxLength = length;
 				}
 				length = 0;
-				sb.Clear();
 				break;
 			}
 		}
