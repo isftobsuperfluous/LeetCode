23fb163633c44965aeca25ad691581ebb381bfb5
isftobsuperfluous <a.kudryavcev8080@gmail.com>
2023-01-28 17:23:15 +0300

    plus 2 solved tasks

---
+++ Medium/3Sum_15/Program.cs
@@ -0,0 +1,57 @@
+﻿var nums = new[] { -1, 0, 1, 2, -1, -4 };
+var threeSumResult = ThreeSum(nums);
+for (var i = 0; i < threeSumResult.Count; i++)
+{
+	for (var j = 0; j < threeSumResult[i].Count; j++)
+	{
+		Console.Write(threeSumResult[i][j] + " ");
+	}
+
+	Console.WriteLine();
+}
+
+IList<IList<int>> ThreeSum(int[] nums)
+{
+	IList<IList<int>> result = new List<IList<int>>();
+
+	if (nums.Length <= 2) return result;
+
+	Array.Sort(nums);
+	int start = 0, left, right, target;
+
+	while (start < nums.Length - 2)
+	{
+		target = nums[start] * -1;
+		left = start + 1;
+		right = nums.Length - 1;
+
+		while (left < right)
+		{
+			if (nums[left] + nums[right] > target)
+			{
+				--right;
+			}
+
+			else if (nums[left] + nums[right] < target)
+			{
+				++left;
+			}
+			else
+			{
+				List<int> OneSolution = new List<int>() { nums[start], nums[left], nums[right] };
+				result.Add(OneSolution);
+
+				while (left < right && nums[left] == OneSolution[1])
+					++left;
+				while (left < right && nums[right] == OneSolution[2])
+					--right;
+			}
+
+		}
+		int currentStartNumber = nums[start];
+		while (start < nums.Length - 2 && nums[start] == currentStartNumber)
+			++start;
+	}
+
+	return result;
+}
---
+++ Medium/LetterCombinationsOfPhoneNumber_17/Program.cs
@@ -0,0 +1,56 @@
+﻿DisplayCombinations(LetterCombinations("23"));
+//DisplayCombinations(LetterCombinations("234"));
+//DisplayCombinations(LetterCombinations("297"));
+//DisplayCombinations(LetterCombinations(string.Empty));
+
+void DisplayCombinations(IList<string> combinations)
+{
+	foreach (var combination in combinations)
+	{
+		Console.Write(combination + " ");
+	}
+
+	Console.WriteLine();
+}
+
+IList<string> LetterCombinations(string digits)
+{
+	if (string.IsNullOrEmpty(digits))
+	{
+		return Array.Empty<string>();
+	}
+	var storage = new Dictionary<int, char[]>()
+	{
+		{0, Array.Empty<char>()},
+		{1, Array.Empty<char>()},
+		{2, new[] {'a', 'b', 'c'}},
+		{3, new[] {'d', 'e', 'f'}},
+		{4, new[] {'g', 'h', 'i'}},
+		{5, new[] {'j', 'k', 'l'}},
+		{6, new[] {'m', 'n', 'o'}},
+		{7, new[] {'p', 'q', 'r', 's'}},
+		{8, new[] {'t', 'u', 'v'}},
+		{9, new[] {'w', 'x', 'y', 'z'}},
+	};
+	var queue = new Queue<string>();
+	queue.Enqueue(string.Empty);
+	var result = new List<string>();
+	while (queue.Count > 0)
+	{
+		var s = queue.Dequeue();
+		if (s.Length == digits.Length)
+		{
+			result.Add(s);
+		}
+		else
+		{
+			var chars = storage[int.Parse(digits[s.Length].ToString())];
+			foreach (var c in chars)
+			{
+				queue.Enqueue(s + c);
+			}
+		}
+	}
+
+	return result;
+}
