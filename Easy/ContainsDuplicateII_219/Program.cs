c4d92f2a4c8c242abe23e98ece74478e6b69d546
isftobsuperfluous <a.kudryavcev8080@gmail.com>
2023-01-29 23:16:45 +0300

    plus 2 solved tasks

---
+++ Easy/ContainsDuplicateII_219/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine(Solution.ContainsNearbyDuplicate(new[] { 1, 0, 1, 1 }, 1));
+
+public class Solution
+{
+	public static bool ContainsNearbyDuplicate(int[] nums, int k)
+	{
+		var dictionary = new Dictionary<int, int>();
+		for (int i = 0; i < nums.Length; i++)
+		{
+			if (!dictionary.TryAdd(nums[i], i))
+			{
+				if (Math.Abs(i - dictionary[nums[i]]) <= k)
+				{
+					return true;
+				}
+
+				dictionary[nums[i]] = i;
+			}
+		}
+
+		return false;
+	}
+}
---
+++ Easy/IsomorphicStrings_205/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine(Solution.IsIsomorphic("paper", "title"));
+
+public class Solution
+{
+	public static bool IsIsomorphic(string s, string t)
+	{
+		if (s.Length != t.Length)
+		{
+			return false;
+		}
+		var ltrMap = new Dictionary<char, char>();
+		var rtlMap = new Dictionary<char, char>();
+		for (var i = 0; i < s.Length; i++)
+		{
+			if (!ltrMap.TryAdd(s[i], t[i]))
+			{
+				if (ltrMap[s[i]] != t[i])
+				{
+					return false;
+				}
+			}
+
+			if (!rtlMap.TryAdd(t[i], s[i]))
+			{
+				if (rtlMap[t[i]] != s[i])
+				{
+					return false;
+				}
+			}
+		}
+
+		return true;
+	}
+}
