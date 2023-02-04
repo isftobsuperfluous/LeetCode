0fe8215a530cdc36bed42196c503a21e65b0f202
isftobsuperfluous <a.kudryavcev8080@gmail.com>
2023-02-04 19:20:07 +0300

    plus 2 solved tasks

---
+++ Easy/ImplementStackUsingQueues_225/Program.cs
@@ -0,0 +1,87 @@
+﻿var stack = new MyStack();
+stack.Push(1);
+stack.Push(2);
+var top1 = stack.Top();
+stack.Push(3);
+var top2 = stack.Top();
+Console.WriteLine(top1);
+Console.WriteLine(top2);
+
+public class MyStack
+{
+	private readonly Queue<int> firstQueue;
+	private readonly Queue<int> secondQueue;
+
+	public MyStack()
+	{
+		firstQueue = new Queue<int>();
+		secondQueue = new Queue<int>();
+	}
+
+	public void Push(int x)
+	{
+		if (firstQueue.Count == 0)
+		{
+			firstQueue.Enqueue(x);
+		}
+		else
+		{
+			secondQueue.Enqueue(x);
+		}
+	}
+
+	public int Pop()
+	{
+		if (secondQueue.Count == 0)
+		{
+			while (firstQueue.Count != 1)
+			{
+				secondQueue.Enqueue(firstQueue.Dequeue());
+			}
+
+			var lastInFirst = firstQueue.Dequeue();
+
+			return lastInFirst;
+		}
+
+		while (secondQueue.Count != 1)
+		{
+			firstQueue.Enqueue(secondQueue.Dequeue());
+		}
+
+		var lastInSecond = secondQueue.Dequeue();
+
+		return lastInSecond;
+	}
+
+	public int Top()
+	{
+		if (secondQueue.Count == 0)
+		{
+			while (firstQueue.Count != 1)
+			{
+				secondQueue.Enqueue(firstQueue.Dequeue());
+			}
+
+			var lastInFirst = firstQueue.Dequeue();
+			secondQueue.Enqueue(lastInFirst);
+
+			return lastInFirst;
+		}
+
+		while (secondQueue.Count != 1)
+		{
+			firstQueue.Enqueue(secondQueue.Dequeue());
+		}
+
+		var lastInSecond = secondQueue.Dequeue();
+		firstQueue.Enqueue(lastInSecond);
+
+		return lastInSecond;
+	}
+
+	public bool Empty()
+	{
+		return firstQueue.Count == 0 && secondQueue.Count == 0;
+	}
+}
---
+++ Medium/FindFirstAndLastPositionOfElementInSortedArray_34/Program.cs
@@ -0,0 +1,66 @@
+﻿var result = SearchRange(new[] { 1, 1, 2 }, 1);
+Console.WriteLine(result[0]);
+Console.WriteLine(result[1]);
+
+int[] SearchRange(int[] nums, int target)
+{
+	if (nums is null || nums.Length == 0)
+	{
+		return new[] { -1, -1 };
+	}
+
+	var firstIndex = FindFirst(nums, target);
+	var lastIndex = FindLast(nums, target);
+
+	return new[] { firstIndex, lastIndex };
+}
+
+int FindFirst(int[] nums, int target)
+{
+	var index = -1;
+	var start = 0;
+	var end = nums.Length - 1;
+	while (start <= end)
+	{
+		var mid = (start + end) / 2;
+		if (nums[mid] == target)
+		{
+			index = mid;
+		}
+		if (nums[mid] >= target)
+		{
+			end = mid - 1;
+		}
+		else
+		{
+			start = mid + 1;
+		}
+	}
+
+	return index;
+}
+
+int FindLast(int[] nums, int target)
+{
+	var index = -1;
+	var start = 0;
+	var end = nums.Length - 1;
+	while (start <= end)
+	{
+		var mid = (start + end) / 2;
+		if (nums[mid] == target)
+		{
+			index = mid;
+		}
+		if (nums[mid] <= target)
+		{
+			start = mid + 1;
+		}
+		else
+		{
+			end = mid - 1;
+		}
+	}
+
+	return index;
+}
