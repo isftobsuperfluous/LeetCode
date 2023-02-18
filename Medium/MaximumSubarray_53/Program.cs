62d428c1741cfae4ddc21d264fad57c891997a07
isftobsuperfluous <a.kudryavcev8080@gmail.com>
2023-02-18 12:29:42 +0300

    plus 1 solved task

---
+++ Medium/MaximumSubarray_53/Program.cs
@@ -0,0 +1,19 @@
+﻿var nums = new[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+Console.WriteLine(MaxSubArray(nums));
+
+int MaxSubArray(int[] nums)
+{
+	var max = int.MinValue;
+	var sum = 0;
+	for (var i = 0; i < nums.Length - 1; i++)
+	{
+		sum += nums[i];
+		max = Math.Max(max, sum);
+		if (sum < 0)
+		{
+			sum = 0;
+		}
+	}
+
+	return max;
+}
