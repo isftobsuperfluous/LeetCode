3028e502961d15837f36ffd278ff0155672f5188
isftobsuperfluous <a.kudryavcev8080@gmail.com>
2023-02-28 15:51:02 +0300

    plus 2 solved tasks

---
+++ Easy/IntersectionOfTwoArrays_349/Program.cs
@@ -0,0 +1,46 @@
+﻿var result = Intersection(new[] { 1, 2, 2, 1 }, new[] { 2, 2 });
+foreach (var i in result)
+{
+	Console.WriteLine(i);
+}
+
+int[] Intersection(int[] nums1, int[] nums2)
+{
+	var numsSet1 = new HashSet<int>();
+	var numsSet2 = new HashSet<int>();
+	foreach (var num1 in nums1)
+	{
+		if (!numsSet1.Contains(num1)) numsSet1.Add(num1);
+	}
+
+	foreach (var num2 in nums2)
+	{
+		if (!numsSet2.Contains(num2)) numsSet2.Add(num2);
+	}
+
+	var intersection = new List<int>();
+	if (numsSet1.Count < numsSet2.Count)
+	{
+		for (int i = 0; i < numsSet1.Count; i++)
+		{
+			var element = numsSet1.ElementAt(i);
+			if (numsSet2.Contains(element))
+			{
+				intersection.Add(element);
+			}
+		}
+	}
+	else
+	{
+		for (int i = 0; i < numsSet2.Count; i++)
+		{
+			var element = numsSet1.ElementAt(i);
+			if (numsSet1.Contains(element))
+			{
+				intersection.Add(element);
+			}
+		}
+	}
+
+	return intersection.ToArray();
+}
---
+++ Easy/RangeSumQueryImmutable_303/Program.cs
@@ -0,0 +1,29 @@
+﻿var numArray = new NumArray(new[] { -2, 0, 3, -5, 2, -1 });
+Console.WriteLine(numArray.SumRange(0, 2));
+Console.WriteLine(numArray.SumRange(2, 5));
+Console.WriteLine(numArray.SumRange(0, 5));
+
+class NumArray
+{
+	private readonly int[] _nums;
+
+	public NumArray(int[] nums)
+	{
+		for (int i = 1; i < nums.Length; i++)
+		{
+			nums[i] += nums[i - 1];
+		}
+
+		_nums = nums;
+	}
+
+	public int SumRange(int left, int right)
+	{
+		if (left == 0)
+		{
+			return _nums[right];
+		}
+
+		return _nums[right] - _nums[left - 1];
+	}
+}
