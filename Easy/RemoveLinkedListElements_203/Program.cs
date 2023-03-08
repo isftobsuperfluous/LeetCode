b8e7a8262da511a82e46c6797a7ba6f426b929ba
isftobsuperfluous <a.kudryavcev8080@gmail.com>
2023-03-08 13:12:32 +0300

    plus 2 solved tasks

---
+++ Easy/RemoveLinkedListElements_203/Program.cs
@@ -0,0 +1,56 @@
+﻿//var input = new ListNode(1);
+//input.next = new ListNode(2);
+//input.next.next = new ListNode(6);
+//input.next.next.next = new ListNode(3);
+//input.next.next.next.next = new ListNode(4);
+//input.next.next.next.next.next = new ListNode(5);
+//input.next.next.next.next.next.next = new ListNode(6);
+
+
+// [7,7,7,7] val = 7
+// [0, 7,7,7,7]
+// 0 -> вторая 7
+// current -> вторая семь
+var input = new ListNode(7);
+input.next = new ListNode(7);
+input.next.next = new ListNode(7);
+input.next.next.next = new ListNode(7);
+var result = RemoveElements(input, 7);
+var current = result;
+while (current is not null)
+{
+	Console.WriteLine(current.val);
+	current = current.next;
+}
+
+ListNode RemoveElements(ListNode head, int val)
+{
+	var plug = new ListNode();
+	plug.next = head;
+	var current = plug;
+	while (current.next is not null)
+	{
+		if (current.next.val == val)
+		{
+			current.next = current.next.next;
+		}
+		else
+		{
+			current = current.next;
+		}
+	}
+
+	return plug.next;
+}
+
+class ListNode
+{
+	public int val { get; set; }
+	public ListNode next { get; set; }
+
+	public ListNode(int val = 0, ListNode next = null)
+	{
+		this.val = val;
+		this.next = next;
+	}
+}
